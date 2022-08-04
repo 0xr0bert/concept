78f63a0058caf55907582008b86dd9eec84e653b
Robert Greener <me@r0bert.dev>
2022-08-04 14:21:43 +0100

    CLIConfig.scala: New case class for configuration
    Signed-off-by: Robert Greener <me@r0bert.dev>

---
+++ src/main/scala/dev/r0bert/concept/CLIConfig.scala
@@ -0,0 +1,10 @@
+package dev.r0bert.concept
+
+/** The configuration for the command-line interface
+  *
+  * @param copyright
+  *   Print the copyright information.
+  */
+final case class CLIConfig(
+    copyright: Boolean = false
+)
