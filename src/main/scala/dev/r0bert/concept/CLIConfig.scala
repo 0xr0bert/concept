d9834a86922d670e6dbf2c3ba11ae6d8f069e4a1
Robert Greener <me@r0bert.dev>
2022-08-04 15:48:01 +0100

    CLIConfig: remove copyright
    Signed-off-by: Robert Greener <me@r0bert.dev>

--- src/main/scala/dev/r0bert/concept/CLIConfig.scala
+++ src/main/scala/dev/r0bert/concept/CLIConfig.scala
@@ -4,12 +4,9 @@ import java.io.File
 
 /** The configuration for the command-line interface
   *
-  * @param copyright
-  *   Print the copyright information.
   * @param behavioursTOML
   *   The file containing the behaviours
   */
 final case class CLIConfig(
-    copyright: Boolean = false,
     behavioursTOML: File = null
 )
