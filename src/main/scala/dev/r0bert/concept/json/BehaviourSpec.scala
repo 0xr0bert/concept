a420f688b11518f9a28501a3139e3032b747c2a0
Robert Greener <me@r0bert.dev>
2022-08-05 15:34:29 +0100

    BehaviourSpec: Add documentation
    Signed-off-by: Robert Greener <me@r0bert.dev>

--- src/main/scala/dev/r0bert/concept/json/BehaviourSpec.scala
+++ src/main/scala/dev/r0bert/concept/json/BehaviourSpec.scala
@@ -5,6 +5,16 @@ import play.api.libs.json.Json
 import play.api.libs.json.Format
 import dev.r0bert.beliefspread.core.BasicBehaviour
 
+/** The specification for a [[Json]] file representing behaviours.
+  *
+  * @param name
+  *   The name of the behaviour.
+  * @param uuid
+  *   The UUID of the behaviour, default uses [[UUID.randomUUID]]
+  * @author
+  *   Robert Greener
+  * @since v0.0.1
+  */
 final case class BehaviourSpec(
     name: String,
     uuid: UUID = UUID.randomUUID()
@@ -21,6 +31,14 @@ final case class BehaviourSpec(
   def toBasicBehaviour: BasicBehaviour = BasicBehaviour(name, uuid)
 }
 object BehaviourSpec {
+
+  /** This allows the data to be read from a JSON file, using default values if
+    * not supplied.
+    *
+    * @author
+    *   Robert Greener
+    * @since v0.0.1
+    */
   implicit val behaviourSpecFormat: Format[BehaviourSpec] =
     Json.using[Json.WithDefaultValues].format[BehaviourSpec]
 }
