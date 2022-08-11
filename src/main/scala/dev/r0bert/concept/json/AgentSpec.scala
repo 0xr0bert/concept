74cb0b4ee3441b2ea508126d73835f6548ccc727
Robert Greener <me@r0bert.dev>
2022-08-11 16:56:44 +0100

    AgentSpec(Test): Implement toBasicAgent
    Signed-of-by: Robert Greener <me@r0bert.dev>

--- src/main/scala/dev/r0bert/concept/json/AgentSpec.scala
+++ src/main/scala/dev/r0bert/concept/json/AgentSpec.scala
@@ -5,18 +5,44 @@ import play.api.libs.json.Json
 import play.api.libs.json.Format
 import dev.r0bert.beliefspread.core.Agent
 import dev.r0bert.beliefspread.core.BasicAgent
+import dev.r0bert.beliefspread.core.Behaviour
+import scala.collection.parallel.CollectionConverters._
 
 /** The specification for a [[Json]] file representing [[Agent]]s.
   *
   * @param uuid
   *   The UUID of the agent, default uses [[UUID.randomUUID]].
+  * @param actions
+  *   The [[Behaviour]]s the agent has performed, indexed by time.
   * @author
   *   Robert Greener
   * @since v0.0.1
   */
 final case class AgentSpec(
-    uuid: UUID = UUID.randomUUID()
-)
+    uuid: UUID = UUID.randomUUID(),
+    actions: Map[Int, UUID] = Map.empty
+) {
+
+  /** Convert this [[AgentSpec]] to a [[Agent]].
+    *
+    * @param behaviours
+    *   The behaviours known to the system.
+    * @return
+    *   The [[Agent]].
+    * @author
+    *   Robert Greener
+    * @since v0.0.1
+    */
+  def toBasicAgent(
+      behaviours: Iterable[Behaviour] = Array.empty[Behaviour]
+  ): BasicAgent =
+    val a = BasicAgent(uuid)
+    val uuidBehaviours = behaviours.par
+      .map(b => (b.uuid, b))
+      .toMap
+    actions.foreach((time, b) => a.setAction(time, Some(uuidBehaviours(b))))
+    a
+}
 
 /** This contains a [[Format]] for [[AgentSpec]]
   *
--- src/test/scala/dev/r0bert/concept/json/AgentSpecTest.scala
+++ src/test/scala/dev/r0bert/concept/json/AgentSpecTest.scala
@@ -3,6 +3,7 @@ package dev.r0bert.concept.json
 import java.util.UUID
 import play.api.libs.json.JsValue
 import play.api.libs.json.Json
+import dev.r0bert.beliefspread.core.BasicBehaviour
 
 class AgentSpecTest extends munit.FunSuite {
   test("Valid UUID") {
@@ -39,5 +40,107 @@ class AgentSpecTest extends munit.FunSuite {
     """)
     val a = json.validate[AgentSpec]
     assert(!a.isSuccess, "JSON should be invalid")
+  }
+
+  test("toBasicAgent with valid UUID") {
+    val u = UUID.randomUUID()
+    val ai = AgentSpec(u)
+    val ao = ai.toBasicAgent()
+
+    assertEquals(ao.uuid, u)
+  }
+
+  test("toBasicAgent with unspecified UUID") {
+    val ai = AgentSpec()
+    val ao = ai.toBasicAgent()
+
+    assertEquals(ao.uuid, ai.uuid)
+  }
+
+  test("Valid UUID, actions") {
+    val uuid = UUID.randomUUID()
+    val b1uuid = UUID.randomUUID()
+    val b2uuid = UUID.randomUUID()
+
+    val json: JsValue = Json.parse(s"""
+    {
+      "uuid": "${uuid.toString()}",
+      "actions": {
+        "1": "$b1uuid",
+        "2": "$b2uuid"
+      }
+    }
+    """)
+    val a = json.validate[AgentSpec]
+    assert(a.isSuccess, "JSON should be valid")
+    assertEquals(a.get.uuid, uuid)
+    assertEquals(a.get.actions.size, 2)
+    assertEquals(a.get.actions(1), b1uuid)
+    assertEquals(a.get.actions(2), b2uuid)
+  }
+
+  test("Unspecified UUID, actions") {
+    val b1uuid = UUID.randomUUID()
+    val b2uuid = UUID.randomUUID()
+    val json: JsValue = Json.parse(s"""
+    {
+      "actions": {
+        "1": "$b1uuid",
+        "2": "$b2uuid"
+      }
+    }
+    """)
+    val a = json.validate[AgentSpec]
+    assert(a.isSuccess, "JSON should be valid")
+    assertNotEquals(
+      a.get.uuid,
+      UUID.fromString("00000000-0000-0000-0000-000000000000")
+    )
+    assertEquals(a.get.actions.size, 2)
+    assertEquals(a.get.actions(1), b1uuid)
+    assertEquals(a.get.actions(2), b2uuid)
+  }
+
+  test("Invalid UUID, actions") {
+    val b1uuid = UUID.randomUUID()
+    val b2uuid = UUID.randomUUID()
+
+    val json: JsValue = Json.parse("""
+    {
+      "uuid": "invalid",
+      "actions": {
+        "1": "$b1uuid",
+        "2": "$b2uuid"
+      }
+    }
+    """)
+    val a = json.validate[AgentSpec]
+    assert(!a.isSuccess, "JSON should be invalid")
+  }
+
+  test("toBasicAgent with valid UUID, actions") {
+    val u = UUID.randomUUID()
+    val behaviours = Array(BasicBehaviour("b1"), BasicBehaviour("b2"))
+    val actions = Map(1 -> behaviours(0).uuid, 2 -> behaviours(1).uuid)
+
+    val ai = AgentSpec(u, actions)
+    val ao = ai.toBasicAgent(behaviours)
+
+    assertEquals(ao.uuid, u)
+    assertEquals(ao.getAction(1).get, behaviours(0))
+    assertEquals(ao.getAction(2).get, behaviours(1))
+  }
+
+  test("toBasicAgent with unspecified UUID, actions") {
+    val behaviours = Array(BasicBehaviour("b1"), BasicBehaviour("b2"))
+    val actions = Map(1 -> behaviours(0).uuid, 2 -> behaviours(1).uuid)
+
+    val ai = AgentSpec(actions = actions)
+    val ao = ai.toBasicAgent(behaviours)
+
+    assertEquals(ao.getAction(1).get, behaviours(0))
+    assertEquals(ao.getAction(2).get, behaviours(1))
+
+    assertEquals(ao.uuid, ai.uuid)
   }
 }
