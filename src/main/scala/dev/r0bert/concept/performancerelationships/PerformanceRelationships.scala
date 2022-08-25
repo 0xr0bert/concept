8dc039c6e7385514737f275f06900405bf2a3573
Robert Greener <me@r0bert.dev>
2022-08-25 13:39:48 +0100

    PerformanceRelationships: Move to own package
    Signed-off-by: Robert Greener <me@r0bert.dev>

--- src/main/scala/dev/r0bert/concept/performancerelationships/PerformanceRelationships.scala
+++ src/main/scala/dev/r0bert/concept/performancerelationships/PerformanceRelationships.scala
@@ -1,7 +1,9 @@
-package dev.r0bert.concept
+package dev.r0bert.concept.performancerelationships
 
 import dev.r0bert.beliefspread.core.Belief
 import dev.r0bert.beliefspread.core.Behaviour
+import dev.r0bert.concept.json.PerformanceRelationshipSpec
+import java.util.UUID
 
 /** A relationship that describes how the holder of the [[Belief]] wants to
   * perform the [[Behaviour]].
