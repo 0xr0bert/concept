4800e24fca6669c5c080f3a5a35f172ce2e9cd73
Robert Greener <me@r0bert.dev>
2022-09-13 16:49:43 +0100

    Runner: Implement perceiveBeliefs
    Signed-off-by: Robert Greener <me@r0bert.dev>

--- src/main/scala/dev/r0bert/concept/Runner.scala
+++ src/main/scala/dev/r0bert/concept/Runner.scala
@@ -10,4 +10,18 @@ package dev.r0bert.concept
   */
 class Runner(
     config: CLIConfig
-) {}
+) {
+
+  /** Perceive the beliefs for every agent.
+    *
+    * @param time
+    *   The current simulation time.
+    * @author
+    *   Robert Greener
+    * @since v0.0.1
+    */
+  def perceiveBeliefs(time: Int): Unit =
+    config.agents.foreach(a =>
+      config.beliefs.foreach(b => a.updateActivation(time, b, config.beliefs))
+    )
+}
