1732c71ddafeb684233cc86ae56ca08d3c53eb75
Robert Greener <me@r0bert.dev>
2022-09-13 16:51:25 +0100

    Runner: Implement tick
    Signed-off-by: Robert Greener <me@r0bert.dev>

--- src/main/scala/dev/r0bert/concept/Runner.scala
+++ src/main/scala/dev/r0bert/concept/Runner.scala
@@ -12,6 +12,17 @@ class Runner(
     config: CLIConfig
 ) {
 
+  /** Tick this time of the simulation (i.e., do everything expected).
+    *
+    * @param time
+    *   The current simulation time.
+    * @author
+    *   Robert Greener
+    * @since v0.0.1
+    */
+  def tick(time: Int): Unit =
+    perceiveBeliefs(time)
+
   /** Perceive the beliefs for every agent.
     *
     * @param time
