7b549a3855f0c4e3b52227ecc8ad779a5438cc46
Robert Greener <me@r0bert.dev>
2022-09-13 16:46:22 +0100

    Runner: Add stub
    Signed-off-by: Robert Greener <me@r0bert.dev>

---
+++ src/main/scala/dev/r0bert/concept/Runner.scala
@@ -0,0 +1,13 @@
+package dev.r0bert.concept
+
+/** This runs the simulation.
+  *
+  * @param config
+  *   The [[CLIConfig]] of the simulation
+  * @author
+  *   Robert Greener
+  * @since v0.0.1
+  */
+class Runner(
+    config: CLIConfig
+) {}
